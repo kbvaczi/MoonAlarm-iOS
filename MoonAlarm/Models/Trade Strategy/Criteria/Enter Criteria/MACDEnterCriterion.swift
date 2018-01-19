8a49516e57a94430c03ef68dd3600e6b419ff2b1
KENNETH VACZI <kvaczi2@gmail.com>
2018-01-19 15:51:57 -0600

    allow increasing trend without cross

--- MoonAlarm/Models/Trade Strategy/Criteria/Enter Criteria/MACDEnterCriterion.swift
+++ MoonAlarm/Models/Trade Strategy/Criteria/Enter Criteria/MACDEnterCriterion.swift
@@ -10,26 +10,22 @@ import Foundation
 
 class MACDEnterCriterion: TradeEnterCriterion {
     
-    var requireIncreasingTrend: Bool = true
+    var increasingTrendPeriod: Int? = 3
+    var requireSignalCross: Bool = true
     
-    override init() { }
-    
-    init(requireTrend: Bool) {
-        self.requireIncreasingTrend = requireTrend
+    init(incTrendFor trendLength: Int?, requireCross: Bool) {
+        self.increasingTrendPeriod = trendLength
+        self.requireSignalCross = requireCross
     }
     
     override func passedFor(snapshot: MarketSnapshot) -> Bool {
-        let sticks = snapshot.candleSticks.suffix(3)
+        let stickCount = self.increasingTrendPeriod ?? 1
+        let sticks = snapshot.candleSticks.suffix(stickCount)
         let startIndex = sticks.startIndex
         let endIndex = sticks.endIndex
         
-        guard   let currentMacdHistogram = sticks.last?.macdHistogram,
-                let prevMacdHistogram = sticks[endIndex - 2].macdHistogram else {
-                    return false
-        }
-        
-        // look for increasing trend
-        if requireIncreasingTrend {
+        // look for increasing trend if required
+        if increasingTrendPeriod != nil {
             for i in (startIndex + 1)..<endIndex {
                 guard   let macdH = sticks[i].macdHistogram,
                         let macdHPrev = sticks[i - 1].macdHistogram else { return false }
@@ -37,8 +33,15 @@ class MACDEnterCriterion: TradeEnterCriterion {
             }
         }
         
-        // MACD Crosses under the signal line, or Histogram goes from + to -        
-        return currentMacdHistogram > 0 && prevMacdHistogram < 0
+        // MACD Crosses under the signal line, or Histogram goes from + to -
+        if requireSignalCross {
+            guard   let currentMacdHistogram = sticks.last?.macdHistogram,
+                    let prevMacdHistogram = sticks[endIndex - 2].macdHistogram
+                    else { return false }
+            return currentMacdHistogram > 0 && prevMacdHistogram < 0
+        }
+        
+        return false
     }
     
 }
