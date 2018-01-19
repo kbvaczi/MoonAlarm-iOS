c3c2366fc318a13f991a4251ef682ec5f2635bdb
KENNETH VACZI <kvaczi2@gmail.com>
2018-01-19 10:04:38 -0600

    added market buy/sell average price

--- MoonAlarm/Models/Market Data/OrderBook.swift
+++ MoonAlarm/Models/Market Data/OrderBook.swift
@@ -24,6 +24,8 @@ class OrderBook {
         self.init(symbol: sym, asks: [Order](), bids: [Order]())
     }
     
+    ////////// PRICING //////////
+    
     var topBidPrice: Double? {
         guard bids.count > 0 else { return nil }
         return bids.first?.price
@@ -41,14 +43,24 @@ class OrderBook {
         return (gap / firstAsk - 1).doubleToPercent
     }
     
+    ////////// RUNWAY / FALLWAY PRICING //////////
+    
     func runwayPrice(forVolume volume: Double) -> Double? {
-        var runwayVolume = volume
+        return  self.edgePrice(forVolume: volume, orders: asks)
+    }
+    
+    func fallwayPrice(forVolume volume: Double) -> Double? {
+        return  self.edgePrice(forVolume: volume, orders: bids)
+    }
+    
+    private func edgePrice(forVolume volume: Double, orders: [Order]) -> Double? {
+        var volumeCounter = volume
         var index = 0
-        while runwayVolume > 0 && index < asks.count {
-            runwayVolume -= asks[index].quantity
+        while volumeCounter > 0 && index < orders.count {
+            volumeCounter -= orders[index].quantity
             index += 1
         }
-        return index > 0 ? asks[index - 1].price : nil
+        return index > 0 ? orders[index - 1].price : nil
     }
     
     func runwayPercent(forVolume volume: Double) -> Percent? {
@@ -57,21 +69,49 @@ class OrderBook {
             return (runwayPrice / firstAskPrice - 1).doubleToPercent
     }
     
-    func fallwayPrice(forVolume volume: Double) -> Double? {
-        var fallwayVolume = volume
-        var index = 0
-        while fallwayVolume > 0 && index < bids.count {
-            fallwayVolume -= bids[index].quantity
-            index += 1
-        }
-        return index > 0 ? bids[index - 1].price : nil
-    }
-    
     func fallwayPercent(forVolume volume: Double) -> Percent? {
         guard   let fallwayPrice = self.fallwayPrice(forVolume: volume),
                 let firstAskPrice = self.firstAskPrice else { return nil }
         return (firstAskPrice / fallwayPrice - 1).doubleToPercent
     }
+    
+    ////////// MARKET BUY/SELL AVERAGE PRICING //////////
+    
+    func marketBuyPrice(forpairVolume volume: Double) -> Double? {
+        return self.marketPriceAvg(forpairVolume: volume, orders: self.asks)
+    }
+    
+    func marketSellPrice(forpairVolume volume: Double) -> Double? {
+        return self.marketPriceAvg(forpairVolume: volume, orders: self.bids)
+    }
+    
+    private func marketPriceAvg(forpairVolume volume: Double, orders: [Order]) -> Double? {
+        var pairVolumeCounter = volume
+        var marketOrders = [Order]()
+        var index = 0
+        while pairVolumeCounter > 0 && index < orders.count {
+            let currentOrder = orders[index]
+            let pairQuantity = currentOrder.price * currentOrder.quantity
+            if pairVolumeCounter > pairQuantity {
+                marketOrders.append(Order(price: currentOrder.price,
+                                          quantity: currentOrder.quantity))
+            } else {
+                let quantityLeft = pairVolumeCounter / currentOrder.price
+                marketOrders.append(Order(price: currentOrder.price,
+                                          quantity: quantityLeft))
+            }
+            pairVolumeCounter -= pairQuantity
+            index += 1
+        }
+        // Able to statisfuly entire order using orderbook
+        guard pairVolumeCounter <= 0 else { return nil }
+        
+        let totalOrdersQuantity = marketOrders.map({$0.quantity}).reduce(0, +)
+        let orderAvgPrice = marketOrders.map({ $0.price * $0.quantity }).reduce(0, +) / totalOrdersQuantity
+        return orderAvgPrice
+    }
+    
+    ////////// UPDATING DATA //////////
     
     func updateData(callback: @escaping () -> Void) {
         let symbolPair = self.symbol + TradeStrategy.instance.tradingPairSymbol
