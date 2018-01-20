a980521e67488dbce24d57c51e4d40728d564aca
KENNETH VACZI <kvaczi2@gmail.com>
2018-01-20 08:17:36 -0600

    fixed calculation

--- MoonAlarm/Models/Market Data/CandleSticks/CandleSticksRSI.swift
+++ MoonAlarm/Models/Market Data/CandleSticks/CandleSticksRSI.swift
@@ -9,26 +9,26 @@
 import Foundation
 
 extension Array where Element : CandleStick {
-
+    
     func currentRSI(_ period: Int = 14) -> Double? {
         
         // RSI needs a minimum of 2 * period to be accurate
         guard self.count > (2 * period) else { return nil }
-
+        
         var initialBullAvg: Double = 0.0
         var initialBearAvg: Double = 0.0
         
         let initialPeriod = self.prefix(period)
         for (index, cStick) in initialPeriod.enumerated() {
-            let prevClosePrice = index > 0 ? self[index - 1].closePrice : cStick.openPrice
+            if index == 0 { continue } // can't get prev close from first
+            let prevClosePrice = self[index - 1].closePrice
             let deltaPrice = cStick.closePrice - prevClosePrice
             
             let isBull = deltaPrice > 0
-            let isBear = deltaPrice < 0
             if isBull {
                 let gain = deltaPrice
                 initialBullAvg += gain / Double(period)
-            } else if isBear {
+            } else {
                 let loss = deltaPrice * -1
                 initialBearAvg += loss / Double(period)
             }
@@ -37,9 +37,10 @@ extension Array where Element : CandleStick {
         var bullSMA = SMA(initialPrice: initialBullAvg, period)
         var bearSMA = SMA(initialPrice: initialBearAvg, period)
         
-        let remainingPeriods = self.dropFirst(period)
+        let remainingPeriods = self.dropFirst(period + 1)
+        let rpStartIndex = remainingPeriods.startIndex
         for (index, cStick) in remainingPeriods.enumerated() {
-            let prevClosePrice = index > 0 ? self[index - 1].closePrice : cStick.openPrice
+            let prevClosePrice = self[index + rpStartIndex - 1].closePrice
             let deltaPrice = cStick.closePrice - prevClosePrice
             
             let isBull = deltaPrice > 0
@@ -55,7 +56,8 @@ extension Array where Element : CandleStick {
         }
         
         guard   let bullSMAValue = bullSMA.currentAvg,
-                let bearSMAValue = bearSMA.currentAvg else { return nil }
+            let bearSMAValue = bearSMA.currentAvg
+            else { return nil }
         
         let rs = bullSMAValue / bearSMAValue
         let rsi = (100 - (100 / (1 + rs)))
@@ -82,5 +84,4 @@ extension Array where Element : CandleStick {
             return self
         }
     }
-
 }
