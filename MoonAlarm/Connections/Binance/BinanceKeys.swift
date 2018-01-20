05d34ac796974cbce8125a66c608c4b56a8844a7
KENNETH VACZI <kvaczi2@gmail.com>
2018-01-20 15:21:33 -0600

    added secret keys

---
+++ MoonAlarm/Connections/Binance/BinanceKeys.swift
@@ -0,0 +1,21 @@
+//
+//  BinanceKeys.swift
+//  MoonAlarm
+//
+//  Created by Kenneth Vaczi on 1/20/18.
+//  Copyright © 2018 Vaczoway Solutions. All rights reserved.
+//
+
+import Foundation
+
+// This file not to be tracked by version control after initial commit.
+
+class BinanceKeys {
+    
+    // TODO: This is an example key.  Replace with your own.
+    static let apiKey: String = "vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A"
+    
+    // TODO: This is an example secret.  Replace with your own.
+    static let apiSecret: String = "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j"
+    
+}
